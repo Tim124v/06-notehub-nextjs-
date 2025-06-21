1724eb38aa43957cf42999880d1712a39e490ec8
Timur Bakiiev <177857078+Tim124v@users.noreply.github.com>
2025-06-22 01:24:12 +0200

    fix: удален неиспользуемый импорт ReactNode из TanStackProvider

--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React, { useState } from 'react';
